e6d7197121eff90751196c64232f540235db736a
Matt Bartl <mattbartl@gmail.com>
2019-11-29 13:27:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,47 +1,82 @@
-function bruteForceTwoSum(array, sum) {
+function bruteForceTwoSum(arr, sum) {
   let pairs = [];
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i+1; j < array.length; j++) {
-      if (array[i] + array[j] === sum) {
-        pairs.push([array[i], array[j]]);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === sum) {
+        pairs.push([arr[i], arr[j]]);
       }
     }
   }
   return pairs;
 };
 
-function binarySearchTwoSum(array, sum) {
+function binarySearchTwoSum(arr, sum) {
   let pairs = [];
-  array.sort();
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i+1; j < array.length; j++) {
-      if (array[i]+array[j] === sum)
-        pairs.push([array[i], array[j]]);
+  arr.sort();
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[i]+arr[j] === sum)
+        pairs.push([arr[i], arr[j]]);
     }
   }
   return pairs;
 }
 
-function binaryMatch(array, sum) {
+function binaryMatch(arr, sum) {
   let pairs = [];
-  array.sort();
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i+1; j < array.length; j++) {
-      if (array[i]+array[j] === sum)
-        return true;
-    }
+  arr.sort();
+  for (let i = 0; i < arr.length; i++) {
+    let copy = arr;
+    const target = copy.splice(i, 1);
+    if (copy.includes(sum-target)) {
+      return true;
+    };
   }
   return false;
 }
 
-function hashTwoSum(array, sum) {
+function hashTwoSum(arr, sum) {
+  const hashPairs = {};
   let pairs = [];
-  array.sort();
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i+1; j < array.length; j++) {
-      if (array[i]+array[j] === sum)
-        pairs.push([array[i], array[j]]);
+
+  for (let i = 0; i < arr.length; i++) {
+    let thisNum = arr[i];
+    hashPairs[i] = thisNum;
+  };
+
+  function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+
+    a.sort();
+    b.sort();
+
+    for (var i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
     }
+    return true;
   }
+
+  let unique = true
+
+  for (let i = 0; i < arr.length; i++) {
+    let diff = sum - arr[i]
+
+    if (Object.values(hashPairs).includes(diff)) {
+      let copy = Object.values(hashPairs)
+      copy.splice(i, 1, 'current target')
+
+      let key = copy.indexOf(diff)
+      let potentialPair = [arr[i], hashPairs[key]].sort()
+
+      for (let i = 0; i < pairs.length; i++) {
+        if (arraysEqual(pairs[i], potentialPair)) unique = false;
+      }
+
+      if (unique) pairs.push(potentialPair)
+    };
+  };
+  
   return pairs;
 }
